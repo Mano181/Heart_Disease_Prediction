a9e57780594f1f94b546bffbdfc4a4c8f5f2e7da
Mano <55449900+Mano181@users.noreply.github.com>
2021-04-20 10:45:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model=pickle.load(open('prediction.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('frontpage.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    
+    
+    -------
+    None.
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction=model.predict(final_features)
+   
+    
+    output = prediction[0]
+    if(output==1):
+        return render_template('frontpage.html',prediction_text='You have risk of having diabetes,Take care your health')
+    else:
+        return render_template('frontpage.html',prediction_text='There is no risk of having risk diabetes,maintain your health')
+    #return render_template('index.html',prediction_text='Employee Salary should be $ {}'.format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
